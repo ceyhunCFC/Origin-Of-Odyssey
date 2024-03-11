f1272256c7400defc5b8aab54aa029ab8dcce6c9
Enes Cesur <enes465enes@gmail.com>
2024-03-11 20:00:57 +0300

    Revize

--- Origin-Of-Odyssey/Assets/Enes Cesur (UI)/MainMenuCanvas.cs
+++ Origin-Of-Odyssey/Assets/Enes Cesur (UI)/MainMenuCanvas.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuCanvas : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject playPanel;
+    public GameObject mainCardsPanel;
+    public GameObject roomPanel;
+    public GameObject gameModelsPanel;
+    public GameObject navbarPanel;
+
+    private GameObject lastOpenedPanel;
+
     void Start()
     {
-        
+        lastOpenedPanel = playPanel;
+        OpenPanel(playPanel);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenCardsPanel()
     {
-        
+        lastOpenedPanel.SetActive(false);
+        OpenPanel(mainCardsPanel);
+    }
+
+    public void OpenRoomPanel()
+    {
+        lastOpenedPanel.SetActive(false);
+        OpenPanel(roomPanel);
+    }
+
+    public void SetupRoom()
+    {
+        lastOpenedPanel.SetActive(false);
+        OpenPanel(gameModelsPanel);
+    }
+
+    public void ClosePanel()
+    {
+        lastOpenedPanel.SetActive(false);
+        OpenPanel(lastOpenedPanel);
+    }
+
+    private void OpenPanel(GameObject panel)
+    {
+        panel.SetActive(true);
+        navbarPanel.SetActive(true);
+        lastOpenedPanel = panel;
     }
 }
